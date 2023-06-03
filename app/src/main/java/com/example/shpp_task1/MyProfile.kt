f32652f626faecaddd8cd94f5a74f87e0df13f87
Iezekiile <iezekiile98@gmail.com>
2023-06-03 20:59:22 +0300

    First commit

---
+++ app/src/main/java/com/example/shpp_task1/MyProfile.kt
@@ -0,0 +1,11 @@
+package com.example.shpp_task1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyProfile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.myprofile_activity)
+    }
+}
