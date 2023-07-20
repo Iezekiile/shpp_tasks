cebf192fce5d16c786f96a57dda16553d4371cf2
Iezekiile <iezekiile98@gmail.com>
2023-07-20 21:37:13 +0300

    Update ContactsAdapter.kt

--- app/src/main/java/com/example/shpp_task1/view/ContactsAdapter.kt
+++ app/src/main/java/com/example/shpp_task1/view/ContactsAdapter.kt
@@ -2,11 +2,10 @@ package com.example.shpp_task1.view
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.AdapterView
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
+
 import com.example.shpp_task1.R
-import com.example.shpp_task1.databinding.ItemUserBinding
+import com.example.shpp_task1.databinding.ItemContactBinding
 import com.example.shpp_task1.model.Contact
 
 class ContactsAdapter : RecyclerView.Adapter<ContactsAdapter.ContactsViewHolder>() {
@@ -21,7 +20,7 @@ class ContactsAdapter : RecyclerView.Adapter<ContactsAdapter.ContactsViewHolder>
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactsViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val binding = ItemUserBinding.inflate(inflater, parent, false)
+        val binding = ItemContactBinding.inflate(inflater, parent, false)
         return ContactsViewHolder(binding)
     }
 
@@ -34,7 +33,7 @@ class ContactsAdapter : RecyclerView.Adapter<ContactsAdapter.ContactsViewHolder>
         }
     }
 
-    inner class ContactsViewHolder(val binding: ItemUserBinding) : RecyclerView.ViewHolder(binding.root){
+    inner class ContactsViewHolder(val binding: ItemContactBinding) : RecyclerView.ViewHolder(binding.root){
         init {
             itemView.setBackgroundResource(R.drawable.bg_view_holder)
         }
